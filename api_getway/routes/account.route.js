d03d65fd5c45a2c811221aabef569a5443462a0d
tampm2 <tampm2@vng.com.vn>
2020-02-07 22:49:50 +0700

    add backend

--- api_getway/routes/account.route.js
+++ api_getway/routes/account.route.js
@@ -4,6 +4,8 @@ const rndToken = require('rand-token')
 const authModel = require('../models/auth.model')
 const common = require('../utils/common')
 const accountModel = require('../models/account.model')
+const receiverModel = require('../models/receiverInfo.model')
+const userModel = require('../models/user.model')
 
 const router = express.Router()
 
@@ -16,6 +18,14 @@ function validate(data) {
     let DoB = data['date_of_birth']
     data['password'] = DoB.split('-').join('')
     return true
+}
+
+function validateReceiverData(data) {
+  if(!common.required(data['id'])) return false
+  if(!common.required(data['accountNum'])) return false
+  if(!common.isNumber(data['accountNum'])) return false
+  
+  return true
 }
 
 router.post('/', async (req, res) => {  
@@ -75,49 +85,44 @@ router.post('/id', async (req, res) => {
 
 // create receiver_info
 router.post('/ref/account', async (req, res) => {  
-  // let info = await accountModel.getInfoBanking(req.body.id)
-  // let errorCode = 400
-  // let ret = {
-  //   msg: 'invalid parameters',
-  // }
-  // if(info && info.length != 0) {
-  //     let saving_money = 0, main = 0
-  //     errorCode = 200
-  //     for (let i = 0; i < info.length; i++) {
-  //       item = info[i]
-  //       if(item.type == 1) main += item.surplus
-  //       else saving_money += item.surplus
-  //     }
-  //     ret = {
-  //       msg: 'successfully',
-  //       saving_money,
-  //       main
-  //     }
-  // }
-  // res.status(errorCode).json(ret)
+  const isValid = validateReceiverData(req.body)
+  let errorCode = 400
+  let ret = {
+    msg: 'invalid parameters',
+  }
+  if(isValid){
+    let user = userModel.singleByAccountNum(req.body.accountNum)
+    let entity = {
+      owner_id: req.body.id,
+      account_id: user.id,
+      alias_name: req.body.aliasName
+    }
+    let newReceiver = await receiverModel.add(entity)
+      errorCode = 200
+      ret = {
+        msg: 'successfully',
+        newReceiver: entity
+      }
+  }
+  res.status(errorCode).json(ret)
 })
 // get all receiver_info by uid
 router.post('/ref/account/id', async (req, res) => {  
-  // let info = await accountModel.getInfoBanking(req.body.id)
-  // let errorCode = 400
-  // let ret = {
-  //   msg: 'invalid parameters',
-  // }
-  // if(info && info.length != 0) {
-  //     let saving_money = 0, main = 0
-  //     errorCode = 200
-  //     for (let i = 0; i < info.length; i++) {
-  //       item = info[i]
-  //       if(item.type == 1) main += item.surplus
-  //       else saving_money += item.surplus
-  //     }
-  //     ret = {
-  //       msg: 'successfully',
-  //       saving_money,
-  //       main
-  //     }
-  // }
-  // res.status(errorCode).json(ret)
+  let receivers = receiverModel.get(req.body.id)
+  let errorCode = 200
+  if(receivers) {
+    ret = {
+      msg: 'successfully',
+      receivers
+    }
+    
+  } else {
+    ret = {
+      msg: 'not found uid',
+      receivers
+    }
+  }
+  res.status(200).json(ret)
 })
   
 module.exports = router
