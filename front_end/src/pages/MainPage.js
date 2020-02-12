2956b5fb3bde20f7a7c7b3cf923f6c92c970c6bd
hmthanh <hmthanhgm@gmail.com>
2020-02-12 21:50:02 +0700

    create page listAccount

--- front_end/src/pages/MainPage.js
+++ front_end/src/pages/MainPage.js
@@ -3,6 +3,14 @@ import { Redirect, Route, Switch } from 'react-router-dom'
 import { Container, Row, Col } from 'reactstrap';
 import Header from '../layout/Header'
 import { Spinner } from 'reactstrap'
+import ListAccountPage from './ListAccountPage/ListAccountPage';
+const LoginPage = lazy(() => import('./Login'))
+const Register = lazy(() => import('./Register'))
+const UserInfo = lazy(() => import('./UserInfo'))
+const HistoryPage = lazy(() => import('./HistoryPage'))
+const ChangePassword = lazy(() => import('./ChangePassword'))
+const ForgetPassword = lazy(() => import('./ForgetPassword'))
+const SettingPage = lazy(() => import('./SettingRecieverPage'))
 
 
 // lazy loading example
@@ -11,39 +19,32 @@ import { Spinner } from 'reactstrap'
 // const BadgePage = React.lazy(() => import('pages/BadgePage'));
 //
 
-const LoginPage =  lazy(() => import('./Login'))
-const Register = lazy(()=>import('./Register'))
-const UserInfo = lazy(()=>import('./UserInfo'))
-const HistoryPage = lazy(()=>import('./HistoryPage'))
-const ChangePassword = lazy(()=>import('./ChangePassword'))
-const ForgetPassword = lazy(()=>import('./ForgetPassword'))
-const SettingPage = lazy(()=>import('./SettingRecieverPage'))
 
 class Main extends Component {
   render() {
     return (
-
       <Container className="themed-container">
         <Header />
         <main className="main">
-        <Suspense fallback={<div>
-          <Spinner type="grow" color="primary" />
-          <Spinner type="grow" color="secondary" />
-          <Spinner type="grow" color="success" />
-          <Spinner type="grow" color="danger" />
-          <Spinner type="grow" color="warning" />
-          <Spinner type="grow" color="info" />
-          <Spinner type="grow" color="light"/>
-      </div>}>
-          <Switch>
-            <Route exact path='/login' component={LoginPage} />
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/info' component={UserInfo} />
-            <Route exact path='/transfer-history' component={HistoryPage} />
-            <Route exact path='/change-password' component={ChangePassword} />
-            <Route exact path='/forget-password' component={ForgetPassword} />
-            <Route exact path='/setting' component={SettingPage} />
-          </Switch>
+          <Suspense fallback={<div>
+            <Spinner type="grow" color="primary" />
+            <Spinner type="grow" color="secondary" />
+            <Spinner type="grow" color="success" />
+            <Spinner type="grow" color="danger" />
+            <Spinner type="grow" color="warning" />
+            <Spinner type="grow" color="info" />
+            <Spinner type="grow" color="light" />
+          </div>}>
+            <Switch>
+              <Route exact path='/login' component={LoginPage} />
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/info' component={UserInfo} />
+              <Route exact path='/transfer-history' component={HistoryPage} />
+              <Route exact path='/change-password' component={ChangePassword} />
+              <Route exact path='/list-account' component={ListAccountPage} />
+              <Route exact path='/forget-password' component={ForgetPassword} />
+              <Route exact path='/setting' component={SettingPage} />
+            </Switch>
           </Suspense>
         </main>
 
