b230f986773bd88c26fda196b69d2ecdb2c09b1d
ngoctham365 <ngoctham365@gmail.com>
2020-03-30 15:37:59 +0700

    guide api

---
+++ api_getway/routes/employee.route.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const moment = require('moment')
+const { getall, getByAccountNum } = require('../models/history.model')
+const router = express.Router()
+
+
+
+router.post('/hist', async (req, res) => {
+    const type = req.body.type;
+    console.log('=================='+ type)
+    const historyData = await getall(type)
+    historyData.map((val, index) => {
+      val.timestamp = moment(val.timestamp).format('HH:mm:ss, DD-MM-YYYY')
+    })
+    res.status(200).json({
+      msg: 'successfully',
+      errorCode: 0,
+      item: historyData
+    });
+
+})
+
+router.post('/hist/:account', async (req, res) => {
+  const type = req.body.type;
+  console.log('=================='+ type)
+  const historyData = await getByAccountNum(req.params.account, type)
+  historyData.map((val, index) => {
+    val.timestamp = moment(val.timestamp).format('HH:mm:ss, DD-MM-YYYY')
+  })
+  res.status(200).json({
+    msg: 'successfully',
+    errorCode: 0,
+    item: historyData
+  });
+
+})
+
+
+module.exports = router;
