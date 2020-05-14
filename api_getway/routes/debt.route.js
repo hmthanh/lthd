fe93d0b85f60a2efef7c7c8b1540d43a165c6d97
ngoctham365 <ngoctham365@gmail.com>
2020-05-14 16:49:57 +0700

    fix wweb socket

--- api_getway/routes/debt.route.js
+++ api_getway/routes/debt.route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const debtModel = require('../models/debt.model')
 const { getInfoByAccount } = require('../models/account.model')
 const router = express.Router()
-const {broadcastAll} = require('../ws')
+const {getInfoByAccountFull} = require('../ws')
 
 // post để lấy tất cả các record trong db. do front end dùng post không dùng get
 router.post('/:id', async (req, res) => {
@@ -35,10 +35,10 @@ router.post('/', async (req, res) => {
     msg
   }
   res.status(errorCode).json(ret)
-  let userInfo =  await getInfoByAccount(req.body.accountNum)
+  let userInfo =  await getInfoByAccountFull(req.body.accountNum)
   if(userInfo.length === 0) {
     let q = req.body.account_num.slice(0, -1)
-    userInfo = await userModel.singleByAccountNum(q)
+    userInfo = await userModel.getInfoByAccountFull(q)
   }
   
   broadcastAll(JSON.stringify({...item, userInfo:userInfo}))
