66e94e8b9051b0767a4b01469b9acf266acc2bd1
hmthanh <hmthanhgm@gmail.com>
2020-05-22 18:59:42 +0700

    update new link url for report

---
+++ front_end/src/pages/CreatePayment/CreatePayment.js
@@ -0,0 +1,145 @@
+import React, {useState} from 'react';
+import {
+  Badge,
+  Button,
+  Card,
+  CardTitle,
+  Col,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  InputGroup,
+  Label,
+  Row
+} from "reactstrap";
+import {useDispatch} from "react-redux";
+import MessageBox from "../../components/Modal/MessageBox";
+import useInputChange from "../../utils/useInputChange";
+import {createAcc} from "../../redux/creators/accountCreator";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import {formatFormalDate} from "../../utils/utils";
+import useToggle from "../../utils/useToggle";
+
+const CreatePayment = () => {
+  const dispatch = useDispatch();
+  const messageBoxToggle = useToggle(false);
+  const [contentMessage, setContentMessage] = useState("");
+  const [titleMessage, setTitleMessage] = useState("");
+  const fullName = useInputChange("");
+  const email = useInputChange("");
+  const phone = useInputChange("");
+  const [dateOfBirth, setDateOfBirth] = useState(new Date());
+
+  function showFieldRequire() {
+    return <Badge color="danger" pill>Yêu cầu</Badge>
+  }
+
+  function onSetDateOfBirth(value) {
+    setDateOfBirth(value);
+  }
+
+  function onCreateAccount(e) {
+    e.preventDefault();
+    let data = {
+      phone: phone.value,
+      email: email.value,
+      name: fullName.value,
+      date_of_birth: formatFormalDate(dateOfBirth),
+    };
+    let accessToken = localStorage.getItem('accessToken');
+    dispatch(createAcc(data, accessToken))
+        .then((response) => {
+          if (response.msg === "successfully") {
+            setTitleMessage("Thành công");
+            setContentMessage("Đã tạo tài khoản thành công !");
+            messageBoxToggle.setActive();
+          }
+        })
+        .catch((e) => {
+          messageBoxToggle.active();
+          setTitleMessage("Thất bại");
+          setContentMessage("Đã xảy ra lỗi trong quá trình tạo tài khoản !");
+          console.log("error", e);
+        });
+  }
+
+  return (
+      <Container>
+        <div className="container-fluid py-3">
+          <Row>
+            <Col xs={12} sm={8} md={6} lg={5} className={"mx-auto"}>
+              <Card id="localBank">
+                <div className="card-body">
+                  <CardTitle>
+                    <h3 className="text-center">TẠO TÀI KHOẢN</h3>
+                  </CardTitle>
+                  <hr/>
+                  <Form method="post" noValidate="novalidate"
+                        className="needs-validation" onSubmit={onCreateAccount}>
+
+                    <h4>Thông tin cá nhân</h4>
+                    <FormGroup>
+                      <Label for="fullName">Họ và tên {showFieldRequire()}</Label>
+                      <InputGroup className="mb-2">
+                        <Input type="text"
+                               name="fullName"
+                               id="fullName"
+                               onChange={fullName.onChange}
+                               value={fullName.value}
+                               placeholder="Nguyễn Văn A"
+                        />
+                      </InputGroup>
+                      <Label for="email">Email {showFieldRequire()}</Label>
+                      <InputGroup className="mb-2">
+                        <Input type="email" name="email" id="email"
+                               onChange={email.onChange}
+                               value={email.value}
+                               placeholder="someone@gmail.com"/>
+                      </InputGroup>
+                      <Label for="phone">Số điện thoại {showFieldRequire()}</Label>
+                      <InputGroup className="mb-2">
+                        <Input type="text" name="phone" id="phone"
+                               onChange={phone.onChange}
+                               value={phone.value}
+                               placeholder="0913-472506"/>
+                      </InputGroup>
+                      <Label for="phone">Ngày sinh {showFieldRequire()}</Label>
+                      <InputGroup className="mb-2">
+                        <DatePicker
+                            className="form-control"
+                            type="text"
+                            name="date_of_birth"
+                            dateFormat="dd-MM-yyyy"
+                            onSelect={onSetDateOfBirth}
+                            onChange={onSetDateOfBirth}
+                            selected={dateOfBirth}
+                        />
+                      </InputGroup>
+                    </FormGroup>
+                    <hr/>
+                    <Button id="btnRecharge" type="submit" color={"success"}
+                            size={"lg"}
+                            block={true}
+                            className="d-flex align-items-center justify-content-center"
+                            disabled={false}>
+                      <span>Tạo tài khoản</span>
+                    </Button>
+                  </Form>
+                  <MessageBox
+                      isOpen={messageBoxToggle.active}
+                      title={titleMessage}
+                      content={contentMessage}
+                      onClose={messageBoxToggle.setInActive}
+                  ></MessageBox>
+                </div>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </Container>
+  )
+};
+
+export default CreatePayment;
--- front_end/src/pages/HistoryTrans/HistoryTrans.js
+++ front_end/src/pages/HistoryTrans/HistoryTrans.js
@@ -1,15 +1,30 @@
 import React, {useCallback, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {Button, Card, CardGroup, Col, Container, Form, FormGroup, Input, InputGroup, InputGroupAddon, InputGroupText, Row} from 'reactstrap'
-import './HistoryAccount.css';
+import {
+  Button,
+  Card,
+  CardGroup,
+  Col,
+  Container,
+  Form,
+  FormFeedback,
+  FormGroup,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Row,
+  Spinner
+} from 'reactstrap'
 import useToggle from "../../utils/useToggle";
 import useInputChange from "../../utils/useInputChange";
 import {getUserDeptHistory, getUserReceiveHistory, getUserTransHistory} from "../../redux/creators/historyTransCreator";
 import TableInfoTransfer from "../../components/Table/TableInfoTransfer";
 import TableInfoDept from "../../components/Table/TableInfoDept";
 import MessageBox from "../../components/Modal/MessageBox";
+import {getAccName} from "../../redux/creators/transferCreator";
 
-const HistoryAccount = () => {
+const HistoryTrans = () => {
   const dispatch = useDispatch();
   const historyDebt = useSelector(state => {
     return state.HistoryDept.data
@@ -20,10 +35,19 @@ const HistoryAccount = () => {
   const receiveHistory = useSelector(state => {
     return state.ReceiveHistory.data
   });
+  const AccName = useSelector((state) => {
+    return state.AccName
+  });
   const search = useInputChange();
   const [titleMsg, setTitleMsg] = useState("");
   const [contentMsg, setContentMsg] = useState("");
   const msgBoxToggle = useToggle(false);
+
+  const [accountNum, setAccountNum] = useState("");
+  const [accValid, setAccValid] = useState(false);
+  const [accInValid, setAccInValid] = useState(false);
+  const [accInValidMsg, setAccInValidMsg] = useState("");
+  const name = useInputChange("");
 
   const showMsgBox = useCallback((title, content) => {
     setTitleMsg(title);
@@ -66,6 +90,39 @@ const HistoryAccount = () => {
     //     });
   }, [dispatch, search, showMsgBox]);
 
+  const onBlurAccountNum = useCallback(() => {
+    if (accountNum.value === "") {
+      setAccInValid(true)
+      setAccInValidMsg("Không được để trống")
+      return false;
+    }
+    let accessToken = localStorage.getItem('accessToken');
+    let data = {
+      query: accountNum
+    }
+    dispatch(getAccName(data, accessToken))
+        .then((response) => {
+          console.log("success response", response)
+          name.setValue(response.account.name)
+          setAccountNum(response.account.account_num)
+          setAccValid(true)
+          setAccInValid(false)
+          setAccInValidMsg("")
+        })
+        .catch((err) => {
+          console.log(err)
+          setAccInValid(true)
+          setAccInValidMsg("Không tìm thấy số tài khoản hoặc username trên")
+          setAccountNum("")
+        })
+
+  }, [accountNum, dispatch, name]);
+
+  function onChangeAccountNum(e) {
+    setAccountNum(e.target.value);
+  }
+
+
   return (
       <Container className="container" style={{marginTop: '20px'}}>
         <Row className="justify-content-center">
@@ -77,19 +134,39 @@ const HistoryAccount = () => {
                   <hr/>
                   <Form method="post" noValidate="novalidate"
                         className="needs-validation" onSubmit={findHistoryAccount}>
+                    <h4>Tìm kiếm theo tài khoản</h4>
                     <FormGroup>
                       <Row>
                         <Col xs={6}>
+                          <InputGroup className="mb-2" xs={6}>
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>Số tài khoản</InputGroupText>
+                            </InputGroupAddon>
+                            <Input type="text"
+                                   name="accountNum"
+                                   id="accountNum"
+                                   onChange={onChangeAccountNum}
+                                   value={accountNum}
+                                   onBlur={onBlurAccountNum}
+                                   invalid={accInValid}
+                                   valid={accValid}
+                                   placeholder="Nhập số tài khoản hoặc username"/>
+                            {
+                              AccName.isLoading ? (<InputGroupAddon addonType="prepend">
+                                <InputGroupText><Spinner color="primary"
+                                                         size={"sm"} role="status"
+                                                         aria-hidden="true"/></InputGroupText>
+                              </InputGroupAddon>) : ""
+                            }
+                            <FormFeedback>{accInValidMsg}</FormFeedback>
+                          </InputGroup>
                           <InputGroup>
                             <InputGroupAddon addonType="prepend">
-                              <InputGroupText>Mã tài khoản</InputGroupText>
+                              <InputGroupText>Họ và tên</InputGroupText>
                             </InputGroupAddon>
-                            <Input id="txtSearch"
-                                   type="text"
-                                   placeholder="2323-2334-2342"
-                                   onChange={search.onChange}
-                                   value={search.value}
-                            ></Input>
+                            <Input type="text" name="name"
+                                   disabled={true}
+                                   value={name.value}/>
                           </InputGroup>
                         </Col>
                         <Col xs={6}>
@@ -101,6 +178,30 @@ const HistoryAccount = () => {
                         </Col>
                       </Row>
                     </FormGroup>
+                    {/*<FormGroup>*/}
+                    {/*  <Row>*/}
+                    {/*    <Col xs={6}>*/}
+                    {/*      <InputGroup>*/}
+                    {/*        <InputGroupAddon addonType="prepend">*/}
+                    {/*          <InputGroupText>Mã tài khoản</InputGroupText>*/}
+                    {/*        </InputGroupAddon>*/}
+                    {/*        <Input id="txtSearch"*/}
+                    {/*               type="text"*/}
+                    {/*               placeholder="2323-2334-2342"*/}
+                    {/*               onChange={search.onChange}*/}
+                    {/*               value={search.value}*/}
+                    {/*        ></Input>*/}
+                    {/*      </InputGroup>*/}
+                    {/*    </Col>*/}
+                    {/*    <Col xs={6}>*/}
+                    {/*      <Button id="btnSearch" type="submit" color={"success"}*/}
+                    {/*              className="btn-search"*/}
+                    {/*              disabled={false}>*/}
+                    {/*        <span>Tìm kiếm</span>*/}
+                    {/*      </Button>*/}
+                    {/*    </Col>*/}
+                    {/*  </Row>*/}
+                    {/*</FormGroup>*/}
                   </Form>
                 </div>
               </Card>
@@ -140,4 +241,4 @@ const HistoryAccount = () => {
 };
 
 
-export default HistoryAccount;
+export default HistoryTrans;
