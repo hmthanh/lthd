c6eb9a6b281dcc927d11c407a33a3c13b5b4cc9e
thangnguyenn <thangnguyen12061996>
2020-05-23 18:16:58 +0700

    fix api

--- api_getway/models/bankingInfo.model.js
+++ api_getway/models/bankingInfo.model.js
@@ -6,10 +6,20 @@ module.exports = {
     return db.add(entity, TABLE_NAME)
   },
 
-  getInfoAccount: id => {
-    return db.load(`
+  getInfoAccount: id =>  db.load(`
       SELECT *
       FROM ${TABLE_NAME}
       WHERE owner_id=${id} AND is_close=${0} ORDER BY type`)
-  }
-};
+  ,
+  update: (entity, condition) => db.patch(entity, condition, TABLE_NAME),
+  getInfoAccountByAccNum: accNum => db.load(`
+      SELECT *
+      FROM ${TABLE_NAME}
+      WHERE account_num=${accNum} AND is_close=0`),
+
+  countAccountAcctivate: id => db.load(`
+  SELECT COUNT(*) as num
+  FROM ${TABLE_NAME} as b JOIN user_account u ON b.owner_id = u.id
+  WHERE u.id=${id} AND is_close=${0}`),
+  
+}
