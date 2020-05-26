a8ae9584159201daf14cd5735ffaaafcf4de8dcc
thangnguyenn <thangnguyen12061996@gmail.com>
2020-05-26 10:47:35 +0700

    fix err

--- front_end/src/shares/baseUrl.js
+++ front_end/src/shares/baseUrl.js
@@ -1,7 +1,7 @@
 
 // export const UrlApi = 'http://localhost:5500';
-export const UrlApi = 'http://192.168.1.28:5500';
+export const UrlApi = 'http://localhost:5500';
 // export const epAccount = '/api/accounts/id';
 // export const getBankAssociate = UrlApi + '/api/associate';
 // export const SocketUrl = 'ws://localhost:6500';
-export const SocketUrl = 'ws://192.168.1.28:6500';
+export const SocketUrl = 'ws://localhost:6500';
